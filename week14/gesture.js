6066e6516fab9113efa3215adf7cbff14f082643
RIGUANG JIN <bosco@RIGUANGs-iMac.local>
2020-09-29 12:52:18 -0400

    part6

--- week14/gesture.js
+++ week14/gesture.js
@@ -81,6 +81,11 @@ element.addEventListener('touchcancel', event => {
 let start = (point, context) => {
     // console.log('start', point.clientX, point.clientY)
     context.startX = point.clientX, context.startY = point.clientY;
+    context.points = [{
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY
+    }]
     
     context.isTap = true;
     context.isPan = false;
@@ -109,7 +114,12 @@ let move = (point, context) => {
         console.log(dx, dy);
         console.log('pan')
     }
-
+    context.points = context.potints.filter(point => Date.now() - point.t < 500)
+    context.points.push({
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY
+    })
     // console.log('move', point.clientX, point.clientY)
     
 }
@@ -125,8 +135,23 @@ let end = (point, context) => {
     if(context.isPress) {
         console.log('pressend')
     }
+    context.points = context.potints.filter(point => Date.now() - point.t < 500)
 
-    // console.log('end', point.clientX, point.clientY)
+    let d, v;
+    if(!context.points.length) {
+        v = 0;
+    } else {
+        d = Math.sqrt((point.clientX - context.points[0].x) ** 2 + 
+            (point.clientY- context.points[0].y) ** 2);
+        v = d / (Date.now() - context.points[0].t);
+    }
+    
+    if(v > 1.5) {
+        context.isFlick = true;
+        console.log("flick")
+    } else {
+        context.isFlick = false;
+    }
     
 }
 let cancel = (point, context) => {
